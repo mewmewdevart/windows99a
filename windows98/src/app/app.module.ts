1d437eed619e0cbcaffc212993d3aeed2b5f72c6
Meowbutu <mewmewdevart@gmail.com>
2023-09-08 16:33:46 -0300

    style: stylized some parts of the menu bar + the start of the clock script

--- windows98/src/app/app.module.ts
+++ windows98/src/app/app.module.ts
@@ -1,24 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu.component';
 import { WindowComponent } from './window/window.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    WindowComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		MenuComponent,
+		WindowComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatIconModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatMenuModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatSelectModule,
+		MatSidenavModule,
+		MatGridListModule,
+		MatCardModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- windows98/src/app/menu/menu.component.ts
+++ windows98/src/app/menu/menu.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.sass']
 })
 export class MenuComponent {
+  timeString: string = ''; 
 
+  updateClock() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    
+    let formatHourClock: string;
+    if (hours >= 12)
+      formatHourClock = 'PM';
+    else
+      formatHourClock = 'AM';
+
+    const formattedHours = (hours % 12 || 12).toString().padStart(2, '0'); // padStart() to format numbers, filling the string/numbers with characters on the left
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+
+    this.timeString = `${formattedHours}:${formattedMinutes} ${formatHourClock}`;
+  }
 }
